b6f5c85a05e7439e5cbf91c460e0a95b22fea4ef
Roman <roman.peskoff@gmail.com>
2026-06-29 00:45:26 +0300

    change interfaces and add dependency

--- internal/domain/group.go
+++ internal/domain/group.go
@@ -10,7 +10,7 @@ type Group struct {
 	MatchScore      float64   `json:"match_score"`
 	ProvidersCount  int       `json:"providers_count"`
 	HotelsCount     int       `json:"hotels_count"`
-	MatchReasons    []byte    `json:"match_reasons,omitempty"` // JSONB — храним как raw JSON
+	MatchReasons    []byte    `json:"match_reasons,omitempty"`
 	CreatedAt       time.Time `json:"created_at"`
 	Hotels          []Hotel   `json:"hotels"`
 }
